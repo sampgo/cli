618b410726dd6c227d6f1bd1ce1b40b297e0701d
alex <38814044+alextwothousand@users.noreply.github.com>
2021-04-17 19:34:04 +0100

    genesis

---
+++ handler/init.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/urfave/cli/v2"
+
+func Init(c *cli.Context) error {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"sampgo-cli/handler"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "sampgo",
+		Usage: "Interacts with the SA-MP server via Go",
+		Commands: []*cli.Command{
+			{
+				Name:   "init",
+				Usage:  "Create a new gomode",
+				Action: handler.Init,
+			},
+			{
+				Name:   "build",
+				Usage:  "Builds your gomode",
+				Action: handler.Build,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
